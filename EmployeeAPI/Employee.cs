953ccd83d516c9af75f351ff40ceb859564e51eb
Lokendra <lokendrakumar16@gmail.com>
2022-01-24 19:12:04 +0530

    Add project files.

---
+++ EmployeeAPI/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeAPI
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int DepartmentId { get; set; }
+        public int DesignationId { get; set; }
+        public int ManagerId { get; set; }
+
+        public float Salary { get; set; }
+
+    }
+}
